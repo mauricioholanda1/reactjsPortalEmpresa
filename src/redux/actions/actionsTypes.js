5f5188525f620c041ec317e7fee55259a0e32e74
Mauricio Holanda <maumauholanda0@gmail.com>
2020-02-07 20:27:31 -0300

    types

---
+++ src/redux/actions/actionsTypes.js
@@ -0,0 +1,11 @@
+export const listaTypes = {
+  GET_LISTA: 'GET_LISTA',
+  GET_LISTA_ERROR: 'GET_LISTA_ERROR',
+  LOADING_LISTA: 'LOADING_LISTA'
+};
+
+// export const userTypes = {
+//   GET_USER: 'GET_USER',
+//   GET_USER_ERROR: 'GET_USER_ERROR',
+//   LOADING_USER: 'LOADING_USER'
+// };
