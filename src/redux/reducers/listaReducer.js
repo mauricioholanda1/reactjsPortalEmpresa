a0dea5f2b4b3c27b2af84356600476eef383e2e5
Mauricio Holanda <maumauholanda0@gmail.com>
2020-02-07 20:28:33 -0300

    Reducer lista

---
+++ src/redux/reducers/listaReducer.js
@@ -0,0 +1,25 @@
+import { listaTypes } from '../actions/actionsTypes';
+
+const initialState = {};
+
+export default function listaReducer(state = initialState, action) {
+  switch (action.type) {
+    case listaTypes.GET_LISTA:
+      return {
+        ...state,
+        listaData: action.listaData
+      };
+
+    case listaTypes.LOADING_LISTA:
+      return {
+        ...state,
+        loading: action.loading
+      };
+
+    case listaTypes.GET_LISTA_ERROR:
+      return { ...state, error: action.error };
+
+    default:
+      return state;
+  }
+}
