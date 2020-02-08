10c138cebdd4ad48fa5ebdcb78dcb528a35e49bb
Mauricio Holanda <maumauholanda0@gmail.com>
2020-02-08 13:57:51 -0300

    Ajustes

--- src/components/header.js
+++ src/components/header.js
@@ -5,25 +5,13 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1
-  }
-}));
-
 export default function Header(props) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar className={classes.appBar}>
           <Typography variant="h6" className={classes.title}>
             {props.title}
           </Typography>
@@ -42,3 +30,20 @@ export default function Header(props) {
     </div>
   );
 }
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    fontFamily: "none"
+  },
+  title: {
+    flexGrow: 1,
+    fontSize: 30
+  },
+  appBar: {
+    backgroundColor: "#e84e0f"
+  }
+}));
