d63ad7655f801ecaaa5d5e6ee9c9f23434a01a11
Mauricio Holanda <maumauholanda0@gmail.com>
2020-02-08 13:57:23 -0300

    page extrato

---
+++ src/components/extratoDebito.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+function ccyFormat(num) {
+  return `${num.toFixed(2)}`;
+}
+
+export default function ExtratoDebito({extratoDebito, calculoDebito}) {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="spanning table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center" colSpan={3}>
+              Tabela de Gastos
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className={classes.title}>Descrição</TableCell>
+            <TableCell className={classes.title} align="right">
+              Data
+            </TableCell>
+            <TableCell className={classes.title} align="right">
+              Valor
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {extratoDebito.map(extratoDebito => (
+            <TableRow key={extratoDebito.description}>
+              <TableCell>{extratoDebito.description}</TableCell>
+              <TableCell align="right">{extratoDebito.data}</TableCell>
+              <TableCell align="right">
+                ${ccyFormat(extratoDebito.valor)}
+              </TableCell>
+            </TableRow>
+          ))}
+
+          <TableRow>
+            <TableCell rowSpan={2} />
+            <TableCell colSpan={1}>Total</TableCell>
+            <TableCell align="right">${ccyFormat(calculoDebito)}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 300
+  },
+  title: {
+    fontWeight: "bold"
+  }
+});
--- src/pages/extrato/index.js
+++ src/pages/extrato/index.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-// import actions from "../../redux/actions";
+import actions from "../../redux/actions";
 import Header from "../../components/header";
+import ExtratoDebito from "../../components/extratoDebito";
 
 class Extrato extends Component {
   constructor(props) {
@@ -10,30 +11,39 @@ class Extrato extends Component {
   }
 
   componentDidMount() {
-    // this.chamadaAPi();
+    this.chamadaAPi();
   }
 
   chamadaAPi = async () => {
-    // const {getLista} = this.props;
-    // await getLista();
+    const {getExtrato} = this.props;
+    await getExtrato();
+  };
+
+  totalDivida = extratoDebito => {
+    return extratoDebito.map(({valor}) => valor).reduce((sum, i) => sum + i, 0);
   };
 
   render() {
-
+    const {extrato} = this.props;
+    console.log("extrato", extrato);
+    const extratoDebito = extrato.extratoData.docs;
+    const calculoDebito = this.totalDivida(extratoDebito);
+    console.log("calculoDebito", calculoDebito);
     return (
       <div>
         <Header title="Portal Empresa - Soma" />
+        <ExtratoDebito extratoDebito={(extratoDebito)} calculoDebito={(calculoDebito)} />
       </div>
     );
   }
 }
 
-// const mapStateToProps = state => ({
+const mapStateToProps = state => ({
+  extrato: state.extrato
+});
 
-// });
+const mapDispatchToProps = dispatch => ({
+  getExtrato: () => dispatch(actions.extrato.get())
+});
 
-// const mapDispatchToProps = dispatch => ({
-  
-// });
-
-export default connect()(Extrato);
+export default connect(mapStateToProps, mapDispatchToProps)(Extrato);
